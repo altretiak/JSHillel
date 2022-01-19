b78b5ba98d1859c21e2dd7770f3701364e55e4ca
Alona Tretiak <alonatretiak@Alonas-MBP.lan>
2022-01-20 00:15:42 +0200

    HT5

--- HomeTasks/HT5/ht5.js
+++ HomeTasks/HT5/ht5.js
@@ -3,12 +3,13 @@ let b = '';
 for (let i = 10; i <= 20; i++) {
 
     a = a + i + ",";
-
+    console.log(a);
 }
 
+for (let i = 20; i >= 10; i--) {
+
+    b = b + i + ",";
+    console.log(b);
+}
 alert(a);
-for (let i = 20; i <= 10; i++) {
-
-    b = b - i + ",";
-}
 alert(b);
